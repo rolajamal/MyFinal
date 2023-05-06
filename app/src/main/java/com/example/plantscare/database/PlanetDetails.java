cdc40d9572c9b2b01088b1dcf7bf18b5eb5a07ba
Awadallah <rm22@smail.ucas.edu.ps>
2023-05-06 14:19:58 +0300

    add PlanetDetails.java class

--- app/src/main/java/com/example/plantscare/database/PlanetDetails.java
+++ app/src/main/java/com/example/plantscare/database/PlanetDetails.java
@@ -2,12 +2,29 @@ package com.example.plantscare.database;
 
 public class PlanetDetails {
     String name;
-
+int id;
+String details;
  public String getName() {
   return name;
  }
 
  public void setName(String name) {
   this.name = name;
+ }
+
+ public int getId() {
+  return id;
+ }
+
+ public void setId(int id) {
+  this.id = id;
+ }
+
+ public String getDetails() {
+  return details;
+ }
+
+ public void setDetails(String details) {
+  this.details = details;
  }
 }
