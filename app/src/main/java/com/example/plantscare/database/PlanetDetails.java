c205f19d0725b76be6f9d9677975526df8b0c93f
Awadallah <rm22@smail.ucas.edu.ps>
2023-05-06 14:14:28 +0300

    add PlanetDetails.java class

---
+++ app/src/main/java/com/example/plantscare/database/PlanetDetails.java
@@ -0,0 +1,13 @@
+package com.example.plantscare.database;
+
+public class PlanetDetails {
+    String name;
+
+ public String getName() {
+  return name;
+ }
+
+ public void setName(String name) {
+  this.name = name;
+ }
+}
